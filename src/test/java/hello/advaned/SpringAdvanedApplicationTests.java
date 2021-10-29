41e346d3277af9a2ac25e526b021cf0257c32e4d
chaenabi <find1086@gmail.com>
2021-10-29 14:57:35 +0900

    Initial commit

---
+++ src/test/java/hello/advaned/SpringAdvanedApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.advaned;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvanedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
