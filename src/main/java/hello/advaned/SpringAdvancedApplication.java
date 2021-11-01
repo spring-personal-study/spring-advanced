388c2e0eba58ba98e0e93980d7f069db9c536745
chaenabi <find1086@gmail.com>
2021-11-01 09:54:47 +0900

    Change Project name(Fix misspelling)

--- src/main/java/hello/advaned/SpringAdvancedApplication.java
+++ src/main/java/hello/advaned/SpringAdvancedApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAdvanedApplication {
+public class SpringAdvancedApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringAdvanedApplication.class, args);
+        SpringApplication.run(SpringAdvancedApplication.class, args);
     }
 
 }
